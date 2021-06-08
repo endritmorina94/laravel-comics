20e15497dadc0ef5be25f96dfc786708c31e2f95
endritmorina94 <endrit.morina@outlook.it>
2021-06-08 16:00:33 +0200

    finito main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,53 @@
 @endsection
 
 @section('main_section')
-    <h1>
-        Hellou
-    </h1>
+    {{-- Inizio Sezion Series --}}
+    <section class="series">
+
+        <div class="container">
+            {{-- Titolo del contenitore --}}
+            <div class="title">
+                <h1>Current Series</h1>
+            </div>
+            {{-- Fine Titolo del contenitore --}}
+
+            {{-- Inizio Contenitore delle Serie --}}
+            <div class="series-container">
+                {{-- Inizio Singola Serie --}}
+                @foreach ($series as $serie)
+                    <div class="serie">
+                        <img src="{{ $serie["thumb"] }}" alt="{{ $serie["title"] }}">
+
+                        <div class="serie-name">
+                            <p>
+                                {{ $serie["series"] }}
+                            </p>
+                        </div>
+                    </div>
+                @endforeach
+                {{-- Fine Singola Serie --}}
+            </div>
+            {{-- Fine Contenitore delle Serie --}}
+
+            <a class="filled-btn" href="#">Load More</a>
+
+        </div>
+    </section>
+    {{-- Fine Sezione Series --}}
+
+    {{-- Inizio Bottom Banner --}}
+    <section class="bottom-banner">
+        <div class="container">
+                <ul>
+                    @foreach ($img_links as $img)
+                        <li>
+                            <img src="{{ asset($img["url"]) }}" alt="Buy Link">
+                            <a href="#">{{$img["title"]}}</a>
+                        </li>
+                    @endforeach
+                </ul>
+        </div>
+    </section>
+    {{-- Fine Bottom Banner --}}
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,35 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
 
 
-    return view('home');
+    $series = config("comics");
+
+    $img_links = [
+        [
+            "url" => "img/buy-comics-digital-comics.png",
+            "title" => "Digital Comics"
+        ],
+        [
+            "url" => "img/buy-comics-merchandise.png",
+            "title" => "DC Merchandis"
+        ],
+        [
+            "url" => "img/buy-comics-subscriptions.png",
+            "title" => "Subscription"
+        ],
+        [
+            "url" => "img/buy-comics-shop-locator.png",
+            "title" => "Comic Shop Locator"
+        ],
+        [
+            "url" => "img/buy-dc-power-visa.svg",
+            "title" => "DC Power Visa"
+        ]
+    ];
+
+    $data = [
+        "series" => $series,
+        "img_links" => $img_links
+    ];
+
+    return view('home', $data);
 })->name("home");
