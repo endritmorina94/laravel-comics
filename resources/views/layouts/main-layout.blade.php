e4044021dabe57b80dacfe09b1fc76b4beb308fa
endritmorina94 <endrit.morina@outlook.it>
2021-06-08 14:42:24 +0200

    finito header

--- resources/views/layouts/main-layout.blade.php
+++ resources/views/layouts/main-layout.blade.php
@@ -1,29 +1,44 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>
-            @yield('title_tag')
-        </title>
 
-        <link rel="stylesheet" href="{{ asset("css/app.css")}}">
-    </head>
-    <body>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        @yield('title_tag')
+    </title>
 
-        {{-- Includo il file header dalla cartella partials --}}
-        @include('partials.header')
+    {{-- Google Font --}}
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
-        <main>
-            {{-- Questo è il segnalibro che indica dove verrà inserito
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@300;400;500;600&display=swap" rel="stylesheet">
+
+    {{-- FontAwesome --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
+
+    {{-- Foglio CSS --}}
+    <link rel="stylesheet" href="{{ asset("css/app.css")}}">
+</head>
+
+<body>
+
+    {{-- Includo il file header dalla cartella partials --}}
+    @include('partials.header')
+
+    <main>
+        {{-- Questo è il segnalibro che indica dove verrà inserito
             il contenuto della section main_content --}}
-            @yield('main_section')
-        </main>
+        @yield('main_section')
+    </main>
 
-        {{-- Includo il file footer dalla cartella partials --}}
-        @include('partials.footer')
+    {{-- Includo il file footer dalla cartella partials --}}
+    @include('partials.footer')
 
-        <script src="{{ asset('js/app.js') }}"></script>
+    {{-- Foglio JS --}}
+    <script src="{{ asset('js/app.js') }}"></script>
 
-    </body>
+</body>
+
 </html>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,64 @@
 <header>
-    header
+
+    <div class="top-bar">
+        <div class="container">
+            <span>
+                DC POWER&trade; VISA&reg;
+            </span>
+
+            <span>
+                ADDITIONAL DC SITES <i class="fas fa-caret-down"></i>
+            </span>
+        </div>
+    </div>
+
+    <nav>
+        <div class="container">
+            <div class="logo-container">
+                <img src="{{ asset("img/dc-logo.png") }}" alt="DC Logo">
+            </div>
+
+            <ul class="menu">
+                <li>
+                    <a href="#">Characters</a>
+                </li>
+                <li class="active">
+                    <a href="#">Comics</a>
+                </li>
+                <li>
+                    <a href="#">Movies</a>
+                </li>
+                <li>
+                    <a href="#">Tv</a>
+                </li>
+                <li>
+                    <a href="#">Games</a>
+                </li>
+                <li>
+                    <a href="#">Collectibles</a>
+                </li>
+                <li>
+                    <a href="#">Videos</a>
+                </li>
+                <li>
+                    <a href="#">Fans</a>
+                </li>
+                <li>
+                    <a href="#">News</a>
+                </li>
+                <li>
+                    <a href="#">
+                        Shop
+                    <i class="fas fa-caret-down"></i>
+                </a>
+                </li>
+            </ul>
+
+            <input type="text" name="" value="" placeholder="Search">
+        </div>
+    </nav>
+
+    <div class="banner"></div>
+
+
 </header>
